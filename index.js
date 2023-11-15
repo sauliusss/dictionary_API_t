abf65dc0aad2d204bb4bf2dd0922d395626ff76e
saulius <sjsaulius@gmail.com>
2023-11-15 15:10:32 +0200

    add word meaning and example html and css

---
+++ index.js
@@ -0,0 +1,4 @@
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result = document.querySelector(".sound");
+
+// https://www.youtube.com/watch?v=PUkgK7TI0x0&ab_channel=CodingArtist
